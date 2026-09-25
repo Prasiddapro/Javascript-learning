7e73f8c4ebf91c514cc35bc94e776c022a3ba31a
prasidda <officialprasidda@gmail.com>
2026-09-25 18:04:42 +0100

    passing a value in a function parameter

---
+++ 04.js
@@ -0,0 +1,28 @@
+function add(a, b){
+    return a + b;
+
+}
+let result = add(10, 20);
+console.log(result);
+
+function substarction(a, b) {
+    return a - b;
+}
+
+let result2 = substarction(30, 10);
+console.log(result2);
+
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+let result3 = multiplication(30, 10);
+console.log(result3);
+
+function division(a, b) {
+    return a / b;
+}
+
+let result4 = division(30, 10);
+console.log(result4);
